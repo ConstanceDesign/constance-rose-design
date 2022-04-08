8ceb57706c32e46a9e083d343c917c9a9b9c8e23
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-04-08 12:24:35 -0400

    about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import coverImage from "../../assets/cover/cover-image.jpg";
+import React from "react";
+import constanceBio from "../../assets/cover/constance-bio.jpg";
 
 function About() {
-
   return (
-    <section className="my-5" >
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+    <section className="my-5">
+      <h1 id="about">Bio</h1>
+      <img
+        src={constanceBio}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      />
       <div className="my-2">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-      </p>
+          In grade two, Constance was pulled aside by her teacher and told that
+          her artwork was the equivialnt to a grade six, and that she should
+          keep drawing and being creative... and so she did!
+        </p>
+
+        <p>
+          Constance is an RGD ("Registered Graphic Designer") in Ontario,
+          Canada. She has worked in creative professions for over 18 years. In
+          addition to a three-year graphic design education from St. Clair
+          College in Windsor, and most recently - a full-stack web development
+          bootcamp with the University of Toronto. She has worked in marketing
+          management, social media management, illustration, photography, and
+          copywriting. She has also directed, wrote and produced an online
+          commercial and tutorial for TecTecTec's ULT-G Satellite GPS Golf
+          Watch.
+        </p>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
