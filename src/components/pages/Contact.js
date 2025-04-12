62c115e0be036b0f58f676e77a3e0ecd825e6f10
ConstanceDesign <info@constance.design>
2025-04-11 23:50:10 -0400

    Contact page update

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -21,96 +21,88 @@ export default function Contact() {
       .then(
         () => {
           alert('Your message has been sent!')
-          window.location.reload(true)
+          window.location.reload()
         },
-        () => {
-          alert('Sorry, your message failed to send, try again.')
+        (error) => {
+          console.error('EmailJS error:', error)
+          alert('Sorry, your message failed to send. Try again.')
         }
       )
-      e.target.reset()
+
+    e.target.reset()
   }
 
   return (
-    <>
-      <div className="container contact-page responsive-cont">
-        <div className="text-zone-contact">
-        {/* <h1 class="animate-charcter">Contact </h1> */}
+    <div className="container contact-page responsive-cont">
+      <div className="text-zone-contact">
         <img className="responsive-img" src={Avatar} alt="Constance" />
         <p className="contact-text">Like how I dev & design?</p>
         <p className="contact-text">Feel free to drop me a line!</p>
-          <h3>
-            <AnimatedLetters2
-              letterClass={letterClass}
-              strArray={[
-                'C',
-                'o',
-                'n',
-                's',
-                't',
-                'a',
-                'n',
-                'c',
-                'e',
-                ' ',
-                'R',
-                'o',
-                's',
-                'e',
-              ]}
-              index={11}
-            />
-          </h3>
-        </div>
-
-        <div>
-          <form ref={form} onSubmit={sendEmail}>
-            <ul>
-              <li className="first-name">
-                <input
-                  placeholder="First Name"
-                  type="text"
-                  name="from_name"
-                  required
-                />
-              </li>
-              <li className="half">
-                <input
-                  placeholder="Last Name"
-                  type="text"
-                  name="from_name"
-                  required
-                />
-              </li>
-              <li className="half">
-                <input
-                  placeholder="Email"
-                  type="email"
-                  name="reply_to"
-                  required
-                />
-              </li>
-              <li>
-                <input
-                  placeholder="Subject"
-                  type="text"
-                  name="subject"
-                  required
-                />
-              </li>
-              <li>
-                <textarea
-                  placeholder="Message"
-                  name="message"
-                  required
-                ></textarea>
-              </li>
-              <li>
-                <input type="submit" className="btnn-hover2 color-8" value="SEND" />
-              </li>
-            </ul>
-          </form>
-        </div>
+        <h3>
+          <AnimatedLetters2
+            letterClass={letterClass}
+            strArray={[
+              'C', 'o', 'n', 's', 't', 'a', 'n', 'c', 'e', ' ',
+              'R', 'o', 's', 'e',
+            ]}
+            index={11}
+          />
+        </h3>
       </div>
-    </>
+
+      <div>
+        <form ref={form} onSubmit={sendEmail}>
+          <ul>
+            <li className="first-name">
+              <input
+                placeholder="First Name"
+                type="text"
+                name="first_name"
+                required
+              />
+            </li>
+            <li className="half">
+              <input
+                placeholder="Last Name"
+                type="text"
+                name="last_name"
+                required
+              />
+            </li>
+            <li className="half">
+              <input
+                placeholder="Email"
+                type="email"
+                name="reply_to"
+                required
+              />
+            </li>
+            <li>
+              <input
+                placeholder="Subject"
+                type="text"
+                name="subject"
+                required
+              />
+            </li>
+            <li>
+              <textarea
+                placeholder="Message"
+                name="message"
+                required
+              ></textarea>
+            </li>
+            <li>
+              <input
+                type="submit"
+                className="btnn-hover2 color-8"
+                value="SEND"
+              />
+            </li>
+          </ul>
+        </form>
+      </div>
+    </div>
   )
 }
+
