e2f73874bba1e6b850152260bf292da6d7f64316
ConstanceDesign <info@constance.design>
2026-04-10 16:52:05 -0400

    nav update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/pages/Home'
-import Direction from './components/pages/Direction'
+// import Direction from './components/pages/Direction'
 import Projects from './components/pages/Projects'
 import Contact from './components/pages/Contact'
 // import ThemeSwitcher from './components/ThemeSwitcher';
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/direction" element={<Direction />} />
+          {/* <Route path="/direction" element={<Direction />} /> */}
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
         </Route>
