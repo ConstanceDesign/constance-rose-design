55ebce507de68fd220e6116a8b95acbe073ee1d3
ConstanceDesign <info@constance.design>
2026-04-10 17:39:02 -0400

    port update

--- api/contact.js
+++ api/contact.js
@@ -58,7 +58,6 @@ module.exports = async function handler(req, res) {
       host: process.env.EMAIL_SERVER_HOST,
       port: Number(process.env.EMAIL_SERVER_PORT),
       secure: Number(process.env.EMAIL_SERVER_PORT) === 465,
-      requireTLS: Number(process.env.EMAIL_SERVER_PORT) === 587,
       auth: {
         user: process.env.EMAIL_SERVER_USER,
         pass: process.env.EMAIL_SERVER_PASSWORD,
